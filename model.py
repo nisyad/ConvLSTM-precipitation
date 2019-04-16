9fa428199327a875ab45def8568ce1f4c35bca01
Sumedh R. Sankhe <sumedh.sankhe@gmail.com>
2019-04-16 17:08:14 -0400

    added batchnormalization and dropout layers

--- model.py
+++ model.py
@@ -12,6 +12,8 @@ from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras.optimizers import Adam
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Dropout
 
 
 def create_model(ip_shape, k_size, lr= 0.001 , dec = 0.0, f1 = 16, f2 = 8, loss = 'mse'):
@@ -32,20 +34,28 @@ def create_model(ip_shape, k_size, lr= 0.001 , dec = 0.0, f1 = 16, f2 = 8, loss 
     model = Sequential([
             ConvLSTM2D(filters = f1,
                        kernel_size = k_size, 
-                       input_shape = ip_shape,                       #activation = act,
+                       input_shape = ip_shape,
+                       activation = 'tanh',
                        padding = 'same',
                        data_format = 'channels_first',
                        return_sequences = True),
+            BatchNormalization(),
+            Dropout(0.5),
             ConvLSTM2D(filters = f2,
-                       kernel_size = k_size,                       #activation = act,
+                       kernel_size = k_size,     
+                       activation = 'tanh',
                        padding = 'same',
                        data_format = 'channels_first',
                        return_sequences = False),
+            BatchNormalization(),
+            Dropout(0.5),
             Conv2D(filters = 1,
                    kernel_size = (1, 1),
                    activation = 'relu',
                    padding = 'same',
-                   data_format = 'channels_first')])
+                   data_format = 'channels_first'),
+            BatchNormalization(),
+            Dropout(0.5)])
             
     adam = Adam(lr = lr, decay = dec)
     model.compile(loss = loss, optimizer = adam, metrics=['accuracy'])
@@ -54,6 +64,7 @@ def create_model(ip_shape, k_size, lr= 0.001 , dec = 0.0, f1 = 16, f2 = 8, loss 
 
 path = os.getcwd()
 data = np.load(glob(path+'\\data\\*')[0])
-x_train, y_train, x_val, y_val = dataprep.prep_data(data = data, validation_split = 0.3, window_size = 20)
+x_train, y_train, x_val, y_val = dataprep.prep_data(data = data, validation_split = 0.3, window_size = 10)
 model = create_model(x_train.shape[1:], (3,3), 0.005, 0.0, 32, 32, 'mse')
 history = model.fit(x_train, y_train, epochs = 50, validation_data = (x_val, y_val))
+
